64e889787a8d7bced349bec09dd7e28c4da48dbc
JullianCalbay <102789848+JullianCalbay@users.noreply.github.com>
2022-04-19 19:17:49 +0800

    Biology Scripts Second Commit

---
+++ Scripts/GlobalInteractableBiology.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Interactable", menuName = "Biology Interactables/New Interactable")]
+public class GlobalInteractableBiology : ScriptableObject
+{
+    //----- Biology Global Interactables Information Container -----//
+    public string Name;
+    public string Description;
+
+    public Sprite ImageDisplay;
+    public AudioClip AudioDesc;
+}
